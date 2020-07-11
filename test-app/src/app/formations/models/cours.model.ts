bc40820e690f1a3c6ec471348207d775f9e95624
nawar <nawarchaib7@gmail.com>
2020-07-11 14:35:44 +0200

    formation routing issue

--- test-app/src/app/formations/models/cours.model.ts
+++ test-app/src/app/formations/models/cours.model.ts
@@ -1,2 +1,13 @@
 export class Cours {
-}
+    id : number;
+    titre: string;
+    cour_id: number;
+    element_id: number;
+    module_designation;
+    niveau_designation;
+    created_by;
+    cours_id;
+    created_at: Date;
+    updated_at: Date;
+  }
+  
--- test-app/src/app/models/menu.model.ts
+++ test-app/src/app/models/menu.model.ts
@@ -1,2 +1,15 @@
+import { Composant } from './composant.model';
 export class Menu {
-}
+    id : number;
+    code: string;
+    designation: string;
+    app_module_id: number;
+    visibilityEtudiant:boolean;
+    visibilityEnseignant:boolean;
+    visibilityAdministratif:boolean;
+    visibilityAdministrateur:boolean;
+    visibilityResponsableFormation:boolean;
+    composants: Composant[];
+    created_at: Date;
+    updated_at: Date;
+  }
